29542f78dabe247e8a405bffc1929e93c13721e1
Borja Manso <borjamanso89@gmail.com>
2016-07-14 13:26:51 +0200

    añadida Interfaz CRUD

---
+++ src/main/java/com/ipartek/formacion/model/CurdAble.java
@@ -0,0 +1,29 @@
+package com.ipartek.formacion.model;
+
+import java.util.List;
+
+import com.ipartek.formacion.pojo.Persona;
+
+/**
+ * Declara las operaciones basicas de CRUD
+ * <ul>
+ * 	<li>Create
+ *  <li>Read
+ *  <li>Update
+ *  <li>Delete
+ * </ul>
+ * @author Curso
+ *
+ */
+public interface CurdAble<P> {
+
+	boolean create(P pojo);
+	
+	List<P> getAll();
+	P getById(long id);
+	
+	boolean update(P pojo);
+	
+	boolean delete(long id);	
+	
+}
---
+++ src/main/java/com/ipartek/formacion/model/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Clases para el MODELO encargadas de trabajar con la Base Datos
+ * <ol>
+ * 	<li>Abrir y Cerrar Conexion
+ * 	<li>DAOs
+ * </ol>
+ * 
+ * @author Curso
+ *
+ */
+package com.ipartek.formacion.model;
