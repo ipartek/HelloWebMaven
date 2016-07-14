ce46961cb8d4d769e844d707897f8e1e22f6e5c1
AndoniLuna <andoniluna22@hotmail.com>
2016-07-14 12:19:54 +0200

    Ejercicios de repaso

---
+++ src/main/java/com/ipartek/formacion/model/CrudAble.java
@@ -0,0 +1,27 @@
+package com.ipartek.formacion.model;
+
+import java.util.List;
+
+/**
+ * Declara las operaciones basicas de CRUD
+ * <ul>
+ * 	<li>Create</li>
+ * 	<li>Read</li>
+ * 	<li>Update</li>
+ * 	<li>Delete</li>
+ * </ul>
+ * @author Andoni Luna Moreno
+ *
+ */
+public interface CrudAble<P> {
+	
+	boolean create(P pojo);
+	
+	List<P> getAll();
+	
+	P getById(long id);
+	
+	boolean update(P pojo);
+	
+	boolean delete(long id);
+}
---
+++ src/main/java/com/ipartek/formacion/model/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Clases para el model encargadas de trabajar con la Base Datos
+ * <ol>
+ * 	<li>Abrir y Cerrar Conexion</li>
+ * 	<li>DAOs</li>
+ * </ol>
+ * @author Curso
+ *
+ */
+package com.ipartek.formacion.model;
