ed592a2f6ca4e23db31da5014d4e687989cf86f9
Daniel <Curso@ForGrb-5.ipartekaula.com>
2016-07-27 12:32:04 +0200

    root

--- src/main/java/com/ipartek/formacion/model/DataBaseConnectionImpl.java
+++ src/main/java/com/ipartek/formacion/model/DataBaseConnectionImpl.java
@@ -12,7 +12,7 @@ public class DataBaseConnectionImpl implements DataBaseConnection {
 	private static final String DRIVER = "com.mysql.jdbc.Driver";
 	private static final String URL = "jdbc:mysql://localhost:3306/hwm";
 	private static final String USER = "root";
-	private static final String PASS = "";
+	private static final String PASS = "root";
 
 	private DataBaseConnectionImpl() {
 		conexion = null;
