8b5df1ad685cf5bae57bc6361359a050ac7a4a0f
anderuraga <ander.uraga@gmail.com>
2017-02-09 17:45:05 +0100

    Es posible cambiar el nombre del proyecto

--- src/main/java/com/ipartek/formacion/Constantes.java
+++ src/main/java/com/ipartek/formacion/Constantes.java
@@ -4,7 +4,7 @@ public class Constantes {
 
 	public static final String SERVER = "localhost";
 	public static final String PORT = "8080";
-	public static final String APP_NAME = "HelloWebMaven";
+	public static final String APP_NAME = "planetas";
 	public static final String WEB_HOME = "http://" + SERVER + ":" + PORT + "/" + APP_NAME + "/";
 
 	// OPERACIONES
