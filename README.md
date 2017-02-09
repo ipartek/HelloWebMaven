56ebfb1a711f60157a93c3b6693a1a4650789a3b
anderuraga <ander.uraga@gmail.com>
2017-02-09 17:37:28 +0100

    cambio servidor a "localhost"

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ instalacion:
 <li>Clonar repositorio en Eclipse</li>
 <li>Importar > Existing Maven Project</li>
 <li>Cambiar desde Properties del proyecto "Deployment Assembly" a "/src/main/webapp"</li>
+<li>Cambiar parametros servidor en com.ipartek.formacion.Constantes</li>
 </ol>
 
--- src/main/java/com/ipartek/formacion/Constantes.java
+++ src/main/java/com/ipartek/formacion/Constantes.java
@@ -2,28 +2,25 @@ package com.ipartek.formacion;
 
 public class Constantes {
 
-	public static final String SERVER   = "192.168.0.16";
-	public static final String PORT     = "8080";
+	public static final String SERVER = "localhost";
+	public static final String PORT = "8080";
 	public static final String APP_NAME = "HelloWebMaven";
-	public static final String WEB_HOME = "http://"+SERVER+":"+PORT+"/"+APP_NAME+"/";
-	
-	//OPERACIONES
-	public static final int OP_LIST   = 0;
-	public static final int OP_DETAIL = 1;	
-	public static final int OP_DELETE = 3;	
+	public static final String WEB_HOME = "http://" + SERVER + ":" + PORT + "/" + APP_NAME + "/";
+
+	// OPERACIONES
+	public static final int OP_LIST = 0;
+	public static final int OP_DETAIL = 1;
+	public static final int OP_DELETE = 3;
 	public static final int OP_SEARCH = 5;
-	public static final int OP_NEW    = 2;
-	//public static final int OP_UPDATE = 4;
+	public static final int OP_NEW = 2;
+	// public static final int OP_UPDATE = 4;
 	public static final int OP_SAVE = 35;
-	
-	
-	//VISTAS
+
+	// VISTAS
 	public static final String VIEW_PLANET_LIST = "planets/list.jsp";
 	public static final String VIEW_PLANET_DETAIL = "planets/detail.jsp";
-	
-	//CONTROLADORES
+
+	// CONTROLADORES
 	public static final String CONTROLLER_PLANETS = "planets";
-	
-	
-	
+
 }
